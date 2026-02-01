d6ac93df28a2902ace6a38858c7f59007788ecec
Silas <agbogbasilas@gmail.com>
2026-02-01 23:47:31 +0100

    First commit

---
+++ src/main/java/com/ifri/XAYA/XayaApplication.java
@@ -0,0 +1,13 @@
+package com.ifri.XAYA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XayaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XayaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ifri/XAYA/XayaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ifri.XAYA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XayaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
